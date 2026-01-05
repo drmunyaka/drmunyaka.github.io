025a6c97c9900f473c45cc9ced715e5a01fc91b5
drmunyaka <154537507+drmunyaka@users.noreply.github.com>
2026-01-05 06:49:58 +0000

    Deploying to gh-pages from @ drmunyaka/drmunyaka.github.io@9e342b7d5b1842310582372276d7e12ed6798fc9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course Links",
@@ -43,13 +36,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/community/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
